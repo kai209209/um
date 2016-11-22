4c05a0cbae6c94f5e834984a0a20175d8001a94a
zky <361561797@qq.com>
2016-11-22 17:52:06 +0800

    修改路由设置

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,10 @@ class ApplicationController < ActionController::Base
 
 
   def after_sign_in_path_for(resource)
-    user_path
+    profile_users_path
   end
 
   def after_sign_up_path_for(resource)
-    user_path
+    profile_users_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   root 'static#index'
 
   devise_for :users
-  resource :user, only: [:show]
+  resources :users, only: [:index,:show] do
+    collection do
+      get :profile
+    end
+  end
+
 end
