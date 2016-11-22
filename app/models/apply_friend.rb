1c887c0b8122e2c00c1afa41d539f1d98c431aae
zky <361561797@qq.com>
2016-11-22 17:42:13 +0800

    设置apply_friend和user之间的关系

--- app/models/apply_friend.rb
+++ app/models/apply_friend.rb
@@ -1,2 +1,8 @@
 class ApplyFriend < ApplicationRecord
+  belongs_to :author, class_name: "User"
+  belongs_to :receiver, class_name: "User"
+
+  enum apply_status: [:apply, :pass, :reject]
+
+  
 end
