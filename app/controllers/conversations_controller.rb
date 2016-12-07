c7ae4e814e165f3c73785e54fda2d3003ce89052
zky <361561797@qq.com>
2016-12-07 17:21:11 +0800

    修改样式，并设置时区

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,7 +2,7 @@ class ConversationsController < ApplicationController
 
   def show
     @conversation = current_user.all_conversations.find(params[:id])
-    @user_messages = @conversation.user_messages
+    @user_messages = @conversation.user_messages.limit(50).order(id: :asc)
     @user_message = UserMessage.new
     respond_to do |format|
       format.js
