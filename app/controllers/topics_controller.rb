cb2537c576043e0bfb13eddbda29fa9e73ec0d41
zky <361561797@qq.com>
2016-12-08 15:26:30 +0800

    添加topics controller 的代码

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,3 +1,51 @@
 class TopicsController < ApplicationController
-  
+  layout 'users'
+  before_action :find_topic, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @topics = Topic.all
+  end
+
+  def show
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = current_user.topics.build(topic_params)
+    if @topic.save
+      redirect_to topic_path(@topic)
+    else
+      render 'new'
+    end
+
+  end
+
+  def edit    
+  end
+
+  def update
+    if @topic.update(topic_params)
+      redirect_to topic_path(@topic)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @topic.destroy
+    redirect_to topics_path
+  end
+
+  private
+
+  def find_topic
+    @topic = Topic.find(params[:id])
+  end
+
+  def topic_params
+    params.require(:topic).permit(:title, :content)
+  end
 end
