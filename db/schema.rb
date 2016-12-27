c425058104a3c8b52d26e8b2a65e411d2900d0fe
zky <361561797@qq.com>
2016-12-27 17:59:44 +0800

    迁移名字信息到user 的name字段

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161227095445) do
+ActiveRecord::Schema.define(version: 20161227095639) do
 
   create_table "apply_friends", force: :cascade do |t|
     t.integer  "author_id"
